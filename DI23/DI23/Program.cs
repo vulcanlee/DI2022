f1bf3ebbaff418066f8dcd0b9e8904f3eb13504e
Vulcan <vulcan.lee@gmail.com>
2021-10-27 21:32:06 +0800

    DI23|請指出以下兩個類別哪些地方違反了 DIP 原則

---
+++ DI23/DI23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DI23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecurityService service = new SecurityService();
+            var isSuccess = service.LoginUser("Vulcan", "000");
+            Console.WriteLine("身分驗證" + (isSuccess == true ? "成功" : "失敗"));
+        }
+    }
+    public class SecurityService
+    {
+        public bool LoginUser(string userName, string password)
+        {
+            LoginService service = new LoginService();
+            return service.ValidateUser(userName, password);
+        }
+    }
+
+    public class LoginService
+    {
+        public bool ValidateUser(string userName, string password)
+        {
+            if (userName == "Vulcan" && password == "123") return true;
+            else return false;
+        }
+    }
+}
