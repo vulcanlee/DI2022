f177a710273412ad6b52007a438b8e0b6d3aad01
Vulcan <vulcan.lee@gmail.com>
2021-10-27 19:55:52 +0800

    DI08|體驗有無相依性注入與使用DI容器的用法

---
+++ DI08/DI08/Phase1/Billing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI08.Phase1
+{
+    public class Billing
+    {
+        public void ProcessPayment(OrderInfo orderInfo)
+        {
+            Console.WriteLine("進行付款");
+        }
+    }
+}
---
+++ DI08/DI08/Phase1/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI08.Phase1
+{
+    public class ProductRepository
+    {
+        public void Save()
+        {
+            Console.WriteLine($"產品資訊已經更新");
+        }
+    }
+}
