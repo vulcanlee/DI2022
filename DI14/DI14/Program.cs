88f648ec5598e4ce9c77ed51893c127402160e3b
Vulcan <vulcan.lee@gmail.com>
2021-10-27 21:13:12 +0800

    DI14|第一版：設計矩形類別與計算面積

---
+++ DI14/DI14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DI14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 4; rectangle.Height = 5;
+            Console.WriteLine($"Rectangle" +
+                $"({rectangle.Width}x{rectangle.Height}) area is " +
+                $"{rectangle.GetArea()}");
+        }
+    }
+    public class Rectangle
+    {
+        protected int _height;
+        protected int _width;
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public int GetArea()
+        {
+            return Height * Width;
+        }
+    }
+}
