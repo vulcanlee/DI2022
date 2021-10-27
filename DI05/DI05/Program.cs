9ffcaa88e8aec3e835e1c8cdebf3c0e05b19518e
Vulcan <vulcan.lee@gmail.com>
2021-10-27 19:24:02 +0800

    DI05|關於 SRP 的使用時機 - 修改後

---
+++ DI05/DI05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DI05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccess.InsertData();
+        }
+    }
+    class DataAccess
+    {
+        public static void InsertData()
+        {
+            Console.WriteLine("Data inserted into database successfully");
+            Logger.WriteLog();
+        }
+    }
+    class Logger
+    {
+        public static void WriteLog()
+        {
+            Console.WriteLine("Logged Time:" + DateTime.Now.ToLongTimeString() +
+                " Log  Data insertion completed successfully");
+        }
+    }
+}
