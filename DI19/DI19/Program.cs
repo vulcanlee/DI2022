1287c347f1e81d4d1b76623a41567ee93a00780e
Vulcan <vulcan.lee@gmail.com>
2021-10-27 21:24:04 +0800

    DI19|採用將介面分開成不同群組之技能練習題目說明

---
+++ DI19/DI19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DI19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public interface IAllInOneCar
+    {
+        void StartEngine();
+        void Drive();
+        void StopEngine();
+        void ChangeEngine();
+    }
+
+    public class Driver : IAllInOneCar
+    {
+        public void ChangeEngine()
+        { throw new NotImplementedException(); }
+
+        public void Drive()
+        { }
+
+        public void StartEngine()
+        { }
+
+        public void StopEngine()
+        { }
+    }
+}
